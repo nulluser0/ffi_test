eba6c759d106f361429cd652e0427bc6f9e6b54e
nulluser0 <diuyueng@gmail.com>
2024-09-30 01:05:31 +1000

    docs

--- src/example_plugin/src/lib.rs
+++ src/example_plugin/src/lib.rs
@@ -7,6 +7,21 @@ const PLUGIN_AUTHOR: &str = "Your Name";
 const PLUGIN_DESCRIPTION: &str = "An example plugin for the server.";
 const PLUGIN_VERSION: &str = "0.1.0";
 
+// Very basic order of events:
+// 1. Plugin is loaded by server's libloading
+// 2. Server calls get_plugin() to get the Plugin struct
+// 3. Plugin struct is stored by server
+// 4. Server calls init() on the Plugin struct
+// 5. Plugin uses host functions to interact with server
+// 6. Server calls tick() on the Plugin struct
+// 7. Plugin uses host functions to interact with server
+// 8. Server calls shutdown() on the Plugin struct
+// 9. Plugin uses host functions to interact with server
+// 10. Plugin is unloaded by server's libloading
+
+/// Type def for API
+/// Host/server functions that the plugin can use.
+/// Plugins will use these functions to interact with the host/server.
 #[repr(C)]
 pub struct HostFunctions {
     pub version: u32,
@@ -15,6 +30,8 @@ pub struct HostFunctions {
     pub get_eventually_modified_number: extern "C" fn() -> u32,
 }
 
+/// Type def for API
+/// Metadata for the plugin.
 #[repr(C)]
 pub struct PluginMetadata {
     pub name: *mut c_char,
@@ -23,6 +40,9 @@ pub struct PluginMetadata {
     pub version: *mut c_char,
 }
 
+/// Type def for API
+/// Plugin functions that the host/server can use.
+/// Plugins must implement these functions, as they are called by the host/server.
 #[repr(C)]
 pub struct Plugin {
     pub metadata: extern "C" fn() -> PluginMetadata,
@@ -31,6 +51,10 @@ pub struct Plugin {
     pub shutdown: extern "C" fn(),
 }
 
+/// Function def for API
+/// Called by the host/server to get the plugin's Plugin struct, containing pointers to its functions.
+/// This is the main entry point for the plugin, serving as the "constructor" for the Plugin struct.
+/// The server will use this information to call the plugin's functions.
 #[no_mangle]
 pub extern "C" fn get_plugin() -> *const Plugin {
     &PLUGIN
